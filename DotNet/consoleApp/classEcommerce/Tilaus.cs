a02dd1100088d0e32ddabd6caae84e46ada12cbb
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-03-31 14:07:20 +0300

    add class

---
+++ DotNet/consoleApp/classEcommerce/Tilaus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Tilaus
+{
+    public DateTime Date { get; set; }
+
+    public string ProductName { get; set; }
+
+	public Asiakas Customer { get; set; }
+
+    public int Amount { get; set; }
+}
