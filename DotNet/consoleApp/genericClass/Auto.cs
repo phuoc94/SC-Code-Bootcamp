50edde59df7a9b8cc663630b5e5ad525b5433055
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-04-09 13:06:23 +0300

    add genericClass

---
+++ DotNet/consoleApp/genericClass/Auto.cs
@@ -0,0 +1,9 @@
+using System;
+public class Auto
+{
+    public int Huippunopeus { get; set; }
+
+    public string Merkki { get; set; }
+
+    public string Malli { get; set; }
+}
---
+++ DotNet/consoleApp/genericClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace genericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> merkkijonot = new List<string>();
+
+            merkkijonot.Add("testi");
+            merkkijonot.Add("testi2");
+            merkkijonot.Add("testi3");
+
+
+            merkkijonot.Remove("testi"); // remove testi
+            merkkijonot.RemoveAt(1); // remove index 1
+
+            foreach (string merkkijono in merkkijonot){
+                Console.WriteLine(merkkijono);
+            };
+
+
+            Auto a1 = new()
+            {
+                Merkki = "Toyota",
+                Malli = "Corolla",
+                Huippunopeus = 190
+            };
+            Auto a2 = new()
+            {
+                Merkki = "Audi",
+                Malli = "A4",
+                Huippunopeus = 230
+            };
+
+            List<Auto> autot = new();
+            autot.Add(a1);
+            autot.Add(a2);
+
+            foreach (Auto auto in autot)
+            {
+                Console.WriteLine(auto.Merkki);
+            }
+        }
+    }
+}
